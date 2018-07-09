b7ca75137643027a213b7b9f0aa79675ac43d611
stamen.peev <stamen.peev@softwaregroup-bg.com>
2018-07-09 18:21:59 +0300

    fix: add sync logic to partial update and full update

--- components/script/methods/edit/index.js
+++ components/script/methods/edit/index.js
@@ -1,17 +1,14 @@
 module.exports = ({type, db}) => {
     return async function edit(msg, $meta) {
-        let { id, data } = msg;
+        const { id, data } = msg;
         const cryptoResult = await this.bus.importMethod('crypto.record.get')({id});
-        let updatedRecord = this.merge({}, cryptoResult.data, data);
+        const updatedRecord = this.merge({}, cryptoResult.data, data);
+        const sync = ($meta.requestHeaders && $meta.requestHeaders['x-sync']) || false;
         if (db) {
             await this.bus.importMethod(`db/${$meta.method}`)(Object.assign({}, updatedRecord, {externalId: id}));
         }
         try {
-            await this.bus.importMethod('crypto.record.update')({
-                id,
-                type,
-                data: updatedRecord
-            });
+            await this.bus.importMethod('crypto.record.update')({id, type, data: updatedRecord, sync});
         } catch (e) {
             if (db) {
                 await this.bus.importMethod(`db/${$meta.method}`)(Object.assign({}, cryptoResult.data, {externalId: id}));
--- components/script/methods/update/index.js
+++ components/script/methods/update/index.js
@@ -1,12 +1,13 @@
 module.exports = ({type, db}) => {
     return async function edit(msg, $meta) {
-        let { id, data } = msg;
+        const { id, data } = msg;
+        const sync = ($meta.requestHeaders && $meta.requestHeaders['x-sync']) || false;
         const cryptoResult = await this.bus.importMethod('crypto.record.get')({id});
         if (db) {
             await this.bus.importMethod(`db/${$meta.method}`)(Object.assign({}, data, {externalId: id}));
         }
         try {
-            await this.bus.importMethod('crypto.record.update')({id, type, data});
+            await this.bus.importMethod('crypto.record.update')({id, type, data, sync});
         } catch (e) {
             if (db) {
                 await this.bus.importMethod(`db/${$meta.method}`)(Object.assign({}, cryptoResult.data, {externalId: id}));
