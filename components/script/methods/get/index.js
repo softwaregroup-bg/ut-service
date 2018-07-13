b897725290cf620f4eb1f7e93934ec1eb4c8bced
stamen.peev <stamen.peev@softwaregroup-bg.com>
2018-07-13 10:27:18 +0300

    fix: improve code

--- components/script/methods/get/index.js
+++ components/script/methods/get/index.js
@@ -1,12 +1,11 @@
 module.exports = ({type}) => {
     return async function get(msg, $meta) {
         const cryptoData = await this.bus.importMethod('crypto.record.getBulk')(msg.id.split(','));
-        return cryptoData.records.reduce((all, record) => {
-            all.push({
+        return cryptoData.records.map(record => {
+            return {
                 id: record.id,
                 data: record.data
-            });
-            return all;
-        }, []);
+            };
+        });
     };
 };
