0e6177ebea8d573a67af931e9e339329dff46ea7
Veneta Hristova <veneta.hristova@softwaregroup-bg.com>
2018-07-20 16:45:17 +0300

    fix: fix typos

--- components/crypto/errors.js
+++ components/crypto/errors.js
@@ -3,19 +3,19 @@ module.exports = (bus) => {
     if (!getError('crypto')) {
         const Crypto = defineError('crypto', null, 'Crypto error', 'error');
         // constraint errors
-        const Constraint = defineError('constraint', Crypto, 'Cryoto constraint error', 'error');
+        const Constraint = defineError('constraint', Crypto, 'Crypto constraint error', 'error');
         defineError('notFound', Constraint, 'Crypto constraint not found');
 
         // index errors
-        const Index = defineError('index', Crypto, 'cryoto constraint error', 'error');
+        const Index = defineError('index', Crypto, 'Crypto index error', 'error');
         defineError('notFound', Index, 'Crypto index not found');
 
         // record errors
-        const Record = defineError('record', Crypto, 'cryoto constraint error', 'error');
+        const Record = defineError('record', Crypto, 'Crypto record error', 'error');
         defineError('notFound', Record, 'Crypto record not found');
 
         // health errors
-        const Health = defineError('health', Crypto, 'cryoto constraint error', 'error');
+        const Health = defineError('health', Crypto, 'Crypto health error', 'error');
         defineError('notFound', Health, 'Crypto health not found');
     }
     return fetchErrors('crypto');
